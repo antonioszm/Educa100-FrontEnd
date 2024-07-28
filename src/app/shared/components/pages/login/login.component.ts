b46ae0bfed2b14fb516a0dfbe1414d75ffb817fd
antonioszm <antonioszm@outlook.com>
2024-07-28 11:17:21 -0300

    feat: implementacao do ngprime e correcao da resolucao da tela

--- src/app/shared/components/pages/login/login.component.ts
+++ src/app/shared/components/pages/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, DividerModule, InputTextModule, FloatLabelModule, PasswordModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })

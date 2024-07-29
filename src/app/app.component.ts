bf384a0a5214a2557044e54d9ed162dddae78ed5
antonioszm <antonioszm@outlook.com>
2024-07-29 15:21:34 -0300

    implementacao da logica de login service de autentificacao e mock de usuarios + modelo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import {ButtonModule} from 'primeng/button'
-
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, CommonModule],
+  imports: [RouterOutlet, ButtonModule, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Educa100-FrontEnd';
--- src/app/shared/components/pages/login/login.component.ts
+++ src/app/shared/components/pages/login/login.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { PasswordModule } from 'primeng/password';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonModule, DividerModule, InputTextModule, FloatLabelModule, PasswordModule],
+  imports: [ButtonModule, DividerModule, InputTextModule, FloatLabelModule, PasswordModule, HttpClientModule, CommonModule, FormsModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email: string = '';
+  senha: string = '';
+  erro: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  verificarLogin() {
+    this.authService.login(this.email, this.senha).subscribe(valido => {
+      if (valido) {
+        this.router.navigate(['/login']); // mudar para a home condizente com o papel
+        console.log("logado")
+      } else {
+        this.erro = 'Email ou senha incorretos';
+      }
+    });
+  }
 }

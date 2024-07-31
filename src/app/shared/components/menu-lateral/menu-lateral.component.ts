3da6b881b17aafa643119d9d706b7e8fab555553
antonioszm <antonioszm@outlook.com>
2024-07-30 21:51:51 -0300

    feat: menu lateral e implementacao dos icones

---
+++ src/app/shared/components/menu-lateral/menu-lateral.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Button, ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { AuthService } from '../../services/auth.service';
+
+
+@Component({
+  selector: 'app-menu-lateral',
+  standalone: true,
+  imports: [SidebarModule, ButtonModule],
+  templateUrl: './menu-lateral.component.html',
+  styleUrl: './menu-lateral.component.scss'
+})
+export class MenuLateralComponent {
+  private authService!: AuthService;
+
+  sidebarVisible: boolean = false;
+  deslogar(){
+    this.authService.logout()
+  }
+  tornaVisivel(){
+    this.sidebarVisible = true
+  }
+}

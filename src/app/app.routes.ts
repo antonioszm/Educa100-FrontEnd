a8652ee4595489361ce66d93ffd32f7b16d9a8ea
antonioszm <antonioszm@outlook.com>
2024-08-02 22:44:42 -0300

    feat: inicio da implementacao do home Docentes/ADM

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './shared/components/pages/login/login.component';
+import { HomeComponent } from './shared/components/pages/home/home.component';
 
 export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    },
+    {
+        path: "home",
+        component: HomeComponent
     }
 ];

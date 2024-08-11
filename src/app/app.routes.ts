042b7191e545c68f0208560d18f58476b81f7242
antonioszm <antonioszm@outlook.com>
2024-08-11 11:48:50 -0300

    criacao do cadastro docente totalmente funcional, e os guards de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,32 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './shared/components/pages/login/login.component';
 import { HomeComponent } from './shared/components/pages/home/home.component';
+import { CadastroDocenteComponent } from './shared/components/pages/cadastro-docente/cadastro-docente.component';
+import { logadoGuard } from './guard/logado.guard';
+import { admGuard } from './guard/adm.guard';
 
 export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "home"
+    },
     {
         path: "login",
         component: LoginComponent
     },
     {
         path: "home",
-        component: HomeComponent
+        component: HomeComponent,
+        canActivate: [logadoGuard]
+    },
+    {
+        path: "cadastro-docente",
+        component: CadastroDocenteComponent,
+        canActivate: [logadoGuard, admGuard]
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
     }
 ];
